5c86fe198155ff2a15e03049831daf971f63c604
crogar <58400421+crogar@users.noreply.github.com>
2021-03-25 13:58:05 -0400

    Update query.sql
    Added query to retrieve all data for employee ID 499942 haha!

--- EmployeeSQL/query.sql
+++ EmployeeSQL/query.sql
@@ -55,3 +55,8 @@ SELECT last_name, COUNT(last_name) AS "frequency"
 FROM employees
 GROUP BY last_name
 ORDER BY "frequency" DESC;
+
+-- BONUS PART (SO FUNNY!!!)
+SELECT * FROM employees
+WHERE emp_no = 499942;
+
